fb91fc94cc3f6fc3ae966b9eeed85cbd3b89cd87
al <a@l>
2025-10-01 23:03:21 -0700

    clean up PC specific tmss

--- tmuxcmd_tw700.sh
+++
@@ -1,6 +0,0 @@
-echo "I am: ${BASH_SOURCE[0]}"
-
-TMUXCMDS="$TMUXCMDS new-window \" bash -c '  cd /cygdrive/c/g/wikipland &&   while [ true ]; do    perl l00httpd.pl;    [ 0 -ne 1 ] || break;   done;   bash'\" \; \
-  split-window -v \" bash -c '  put.bat; bash'\" \; rename-window wiki \;"
-TMUXCMDS="$TMUXCMDS new-window \" bash -c '  while [[ 1 ]]; do    \perl /cygdrive/c/g/l00info/bin/pi.pl;    sleep 3;   done;   bash'\" \; rename-window pi \;"
-
